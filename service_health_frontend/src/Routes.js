e9c64f7aff12f0ef1c397b60243a4cf65dbab20e
Nicholas Ting <nicholasting92@gmail.com>
2020-12-06 17:29:17 +1100

    feat: front end authentication done

--- service_health_frontend/src/Routes.js
+++ service_health_frontend/src/Routes.js
@@ -2,12 +2,20 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Home from "./containers/Home/Home";
 import NotFound from "./containers/Errors/NotFound/NotFound";
+import Signup from "./containers/Signup/Signup";
+import Login from "./containers/Login/Login";
 
 export default function Routes() {
   return (
     <Switch>
       <Route exact path="/">
         <Home />
+      </Route>
+      <Route exact path="/login">
+        <Login />
+      </Route>
+      <Route exact path="/signup">
+        <Signup />
       </Route>
       <Route>
         <NotFound />
